ce3d7a5071e76ce18a6d65d164c95994ade01fe9
Fedayizada <rfedayiz@f8ffc23b4973.ant.amazon.com>
2021-01-03 02:54:01 -0500

    deploy

--- images.sh
+++
@@ -1,5 +0,0 @@
-docker manifest inspect  "004889159502.dkr.ecr.us-east-1.amazonaws.com/tea:12" | jq -c '.manifests[]' | while read i; do
-    # do stuff with $i
-     echo $i | jq .platform.architecture
-    
-done
